66d3c1425ee51ac89b45c1ae47ef0227ecc04227
Windows (Sergio Kanno) <sergio.kanno@outlook.com.br>
2025-09-21 17:08:12 -0300

    Altera server.js

--- src/server.js
+++ src/server.js
@@ -19,5 +19,5 @@ server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
   console.log("Boas-vindas à API do AllBooks")
-  console.log("API disponível através da url http://localhost:8000")
+  console.log("API disponível em http://localhost:8000")
 })
